63e4cd3db22a76affb1b2d63fa56b4288bcade96
DeepAQ <i@aq741.com>
2017-12-21 01:21:09 +0800

    HW2: start project with embedded Tomcat

---
+++ 2-Servlet_Session_JDBC/src/main/java/cn/imaq/order/Main.java
@@ -0,0 +1,32 @@
+package cn.imaq.order;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.WebResourceRoot;
+import org.apache.catalina.startup.Tomcat;
+import org.apache.catalina.webresources.DirResourceSet;
+import org.apache.catalina.webresources.JarResourceSet;
+import org.apache.catalina.webresources.StandardRoot;
+
+import java.io.File;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Tomcat tomcat = new Tomcat();
+        tomcat.getHost().setAppBase(".");
+        Context ctx = tomcat.addWebapp("", ".");
+
+        // workaround for Tomcat 9.0
+        WebResourceRoot res = new StandardRoot(ctx);
+        File classPath = new File(Main.class.getProtectionDomain().getCodeSource().getLocation().toURI());
+        if (classPath.isDirectory()) {
+            res.addPreResources(new DirResourceSet(res, "/WEB-INF/classes", classPath.getAbsolutePath(), "/"));
+        } else {
+            res.addPreResources(new JarResourceSet(res, "/WEB-INF/classes", classPath.getAbsolutePath(), "/"));
+        }
+        ctx.setResources(res);
+        tomcat.getConnector();
+
+        tomcat.start();
+        tomcat.getServer().await();
+    }
+}
